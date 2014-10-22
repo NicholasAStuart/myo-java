84da1440c4eea0f07c4496ac16a2f43293cc28e3
Nicholas A. Stuart <nicholasastuart@gmail.com>
2014-10-22 13:40:25 -0700

    Added in OSX libraries. Works for OSX 10.10

--- src/main/java/com/thalmic/myo/example/HelloMyo.java
+++ src/main/java/com/thalmic/myo/example/HelloMyo.java
@@ -10,29 +10,29 @@ import com.thalmic.myo.Myo;
  -XX:+UseOSErrorReporting
  */
 public class HelloMyo {
-    public static void main(String[] args) {
-	try {
-	    Hub hub = new Hub("com.example.hello-myo");
+	public static void main(String[] args) {
+		try {
+			Hub hub = new Hub("com.example.hello-myo");
 
-	    System.out.println("Attempting to find a Myo...");
-	    Myo myo = hub.waitForMyo(10000);
+			System.out.println("Attempting to find a Myo...");
+			Myo myo = hub.waitForMyo(10000);
 
-	    if (myo == null) {
-		throw new RuntimeException("Unable to find a Myo!");
-	    }
+			if (myo == null) {
+				throw new RuntimeException("Unable to find a Myo!");
+			}
 
-	    System.out.println("Connected to a Myo armband!");
-	    DataCollector dataCollector = new DataCollector();
-	    hub.addListener(dataCollector);
+			System.out.println("Connected to a Myo armband!");
+			DataCollector dataCollector = new DataCollector();
+			hub.addListener(dataCollector);
 
-	    while (true) {
-		hub.run(1000 / 20);
-//		System.out.print(dataCollector);
-	    }
-	} catch (Exception e) {
-	    System.err.println("Error: ");
-	    e.printStackTrace();
-	    System.exit(1);
+			while (true) {
+				hub.run(1000 / 20);
+				System.out.print(dataCollector);
+			}
+		} catch (Exception e) {
+			System.err.println("Error: ");
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
-    }
 }
