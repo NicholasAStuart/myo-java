19eebfcff604038c1326c0e90ab2b4933ff82814
Nicholas A. Stuart <nicholasastuart@gmail.com>
2014-12-20 11:26:16 -0800

    Updated to the latest version of Myo, 0.8.0. Added new methods that allow for the collection of EMG data.

--- src/main/java/com/thalmic/myo/example/EmgDataSample.java
+++ src/main/java/com/thalmic/myo/example/EmgDataSample.java
@@ -3,17 +3,12 @@ package com.thalmic.myo.example;
 import com.thalmic.myo.DeviceListener;
 import com.thalmic.myo.Hub;
 import com.thalmic.myo.Myo;
+import com.thalmic.myo.enums.StreamEmgType;
 
-/*
- * JVM Arguments to help debug.
- -Xcheck:jni
- -XX:+ShowMessageBoxOnError
- -XX:+UseOSErrorReporting
- */
-public class HelloMyo {
+public class EmgDataSample {
 	public static void main(String[] args) {
 		try {
-			Hub hub = new Hub("com.example.hello-myo");
+			Hub hub = new Hub("com.example.emg-data-sample");
 
 			System.out.println("Attempting to find a Myo...");
 			Myo myo = hub.waitForMyo(10000);
@@ -21,14 +16,15 @@ public class HelloMyo {
 			if (myo == null) {
 				throw new RuntimeException("Unable to find a Myo!");
 			}
-
+			
 			System.out.println("Connected to a Myo armband!");
-			DeviceListener dataCollector = new DataCollector();
+			myo.setStreamEmg(StreamEmgType.STREAM_EMG_ENABLED);
+			DeviceListener dataCollector = new EmgDataCollector();
 			hub.addListener(dataCollector);
 
 			while (true) {
 				hub.run(1000 / 20);
-				System.out.print(dataCollector);
+				System.out.println(dataCollector);
 			}
 		} catch (Exception e) {
 			System.err.println("Error: ");
--- src/main/java/com/thalmic/myo/example/MultipleMyos.java
+++ src/main/java/com/thalmic/myo/example/MultipleMyos.java
@@ -4,20 +4,20 @@ import com.thalmic.myo.DeviceListener;
 import com.thalmic.myo.Hub;
 
 public class MultipleMyos {
-    public static void main(String... args) {
-	try {
-	    Hub hub = new Hub("com.example.multiple-myos");
+	public static void main(String... args) {
+		try {
+			Hub hub = new Hub("com.example.multiple-myos");
 
-	    DeviceListener printer = new PrintMyoEvents();
-	    hub.addListener(printer);
+			DeviceListener printer = new PrintMyoEvents();
+			hub.addListener(printer);
 
-	    while (true) {
-		hub.run(10);
-	    }
-	} catch (Exception e) {
-	    System.err.println("Error: ");
-	    e.printStackTrace();
-	    System.exit(1);
+			while (true) {
+				hub.run(10);
+			}
+		} catch (Exception e) {
+			System.err.println("Error: ");
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
-    }
 }
